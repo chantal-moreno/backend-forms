8d707eae4406992fed727cf18630a4e22d56fc8e
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2024-10-14 14:13:18 -0600

    Template model

---
+++ models/templateModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema(
+  {
+    questionTitle: { type: String, required: true },
+    questionDescription: { type: String, required: true },
+    questionType: { type: String, required: true },
+    options: [String], // Options multiple choice questions
+    order: { type: Number, required: true },
+  },
+  { _id: true }
+);
+
+const TemplateSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    questions: [questionSchema],
+    topic: { type: String },
+    tags: [String],
+    image: { type: String },
+    isPublic: { type: Boolean, default: true },
+    allowedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Templates', TemplateSchema);
