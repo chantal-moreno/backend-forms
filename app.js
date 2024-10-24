e5ba4196320c0a8dcce0f2a7aad824d88888ac1e
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2024-10-24 17:20:39 -0600

    Update CORS

--- app.js
+++ app.js
@@ -3,30 +3,40 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const routes = require('./index');
 const dbConnect = require('./dbConnect');
+const cors = require('cors');
 
 dbConnect();
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-// CORS (Cross-Origin Resource Sharing)
-app.use((request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  response.setHeader('Access-Control-Allow-Credentials', 'true');
-  response.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-  );
-  response.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  );
-  if (request.method === 'OPTIONS') {
-    return response.sendStatus(200);
-  }
-  next();
-});
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://proyect-forms.vercel.app',
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    // allow postman
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content',
+    'Accept',
+    'Content-Type',
+    'Authorization',
+  ],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(routes);
